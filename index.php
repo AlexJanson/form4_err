c98b2fa4a99825746b4a3b40b674fd72fc5b1b73
AlexJanson <Alex@hoofddorp.nl>
2018-03-05 09:09:58 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+if (isset($_GET['sendContact'])) {
+    include './views/response.php';
+} else
+    include './views/from.php';
+
---
+++ views/response.php
@@ -0,0 +1,5 @@
+<?php
+echo '<h2>Hallo wereld! </h2>';
+echo 'Mijn naam is '.$_GET['name'].'<br/ >';
+echo 'Mijn e-mail is '.$_GET['email'].'<br/ >';
+echo 'Ik zeg: '.$_GET['message'];
